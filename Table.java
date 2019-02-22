8c8dd3345a3b428127a90703ffb60cbd516fc085
Ravi Verma <ravi.cs143p@gmail.com>
2019-02-21 23:27:30 -0500

    modifing the class

--- Table.java
+++ Table.java
@@ -4,5 +4,14 @@ public class Table{
 		
 		System.out.println("Inside Table Class");
 		
+		for (int i=0; i<10; i++){
+			int j = i+1;
+			if (i%2 == 0 ){
+				System.out.println("I love you Preeti, "+ j);
+			} else {
+				System.out.println("I love you Guggu, "+ j);
+			}	
+		}	
+		
 	}
 }
